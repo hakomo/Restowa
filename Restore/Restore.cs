cd209e22c5dcd44b9b1fcec1b0c7d03341065473
hakomo <hakomoa@gmail.com>
2014-06-27 23:33:05 +0900

    first commit

---
+++ Restore/Restore.cs
@@ -0,0 +1,30 @@
+﻿using Hakomo.Library;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Windows.Forms;
+
+namespace Restore {
+
+    class Restore {
+
+        private static WinAPI.Placement[] File {
+            get {
+                FileStream fs = null;
+                try {
+                    fs = new FileStream(Application.StartupPath + "\\restowa.dat", FileMode.Open);
+                    return (WinAPI.Placement[])(new DataContractJsonSerializer(typeof(WinAPI.Placement[]))).ReadObject(fs);
+                } catch {
+                    return new WinAPI.Placement[0];
+                } finally {
+                    if(fs != null)
+                        fs.Dispose();
+                }
+            }
+        }
+
+        private static void Main() {
+            foreach(WinAPI.Placement p in File)
+                p.Restore();
+        }
+    }
+}
---
+++ Save/Save.cs
@@ -0,0 +1,27 @@
+﻿using Hakomo.Library;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Windows.Forms;
+
+namespace Save {
+
+    class Save {
+
+        private static void Main() {
+            List<WinAPI.Placement> placements = new List<WinAPI.Placement>();
+            foreach(IntPtr hw in WinAPI.GetTasks())
+                placements.Add(new WinAPI.Placement(hw));
+            FileStream fs = null;
+            try {
+                fs = new FileStream(Application.StartupPath + "\\restowa.dat", FileMode.Create);
+                (new DataContractJsonSerializer(typeof(WinAPI.Placement[]))).WriteObject(fs, placements.ToArray());
+            } catch {
+            } finally {
+                if(fs != null)
+                    fs.Dispose();
+            }
+        }
+    }
+}
